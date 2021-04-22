6742664b5cda4d2aff70ecb17f559c4d1230675d
Aravindh <aravindhbme@gmail.com>
2021-04-22 14:34:19 +0530

    first commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            if(age<18)
+                { Console.WriteLine("Can't vote"); }
+            else
+                { Console.WriteLine("Can vote"); }
+            Console.ReadLine();
+        }
+    }
+}
