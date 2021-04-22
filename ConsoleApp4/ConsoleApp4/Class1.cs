1af71fc2b4145af856a3f5e9bc22e71f68901356
Aravindh <aravindhbme@gmail.com>
2021-04-22 14:53:15 +0530

    first commit

--- ConsoleApp4/ConsoleApp4/Class1.cs
+++ ConsoleApp4/ConsoleApp4/Class1.cs
@@ -8,5 +8,16 @@ namespace ConsoleApp4
 {
     class Class1
     {
+        int number;
+        Console.WriteLine("Enter age:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num==18 and num<60)
+                { Console.WriteLine( "eligible"); }
+             else if(num>60)
+                { Console.WriteLine(num+ "Senior citizen"); }
+             else 
+                { Console.WriteLine("Not eligible"); }
+          
+                 Console.ReadLine();
     }
 }
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter age:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num == 18 || num<60)
+                { Console.WriteLine("eligible"); }
+            else if (num > 60)
+                 { Console.WriteLine( "Senior citizen"); }
+            else
+                { Console.WriteLine("Not eligible"); }
+
+            Console.ReadLine();
+        }
+    }
+}
