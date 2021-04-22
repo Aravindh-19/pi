eb958310fdda4e0c092524f392a5eba184684d5f
Aravindh <aravindhbme@gmail.com>
2021-04-22 12:21:03 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 8;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+            
+        }
+    }
+}
